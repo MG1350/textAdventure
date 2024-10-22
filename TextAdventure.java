baec7b6669d91c76c4bc5c46ce78f9c9edea0e68
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-10-22 06:32:12 -0400

    Formatting

--- TextAdventure.java
+++ TextAdventure.java
@@ -11,27 +11,17 @@ public class TextAdventure
     console = new FancyConsole("Great Text Adventure!", 600, 600);
     inScanner = new Scanner(System.in);
 
-    // feel free to change the player's starting values
     ourHero = new Player("Bob", 100, 0);
   }
 
   public void play()
   {
     String input;
-    // start of adventure. You can change this if you like
     console.setImage("forest.jpg");
-
-    // ask the user for their name.
     System.out.println("What is your name?\n");
     input = inScanner.nextLine();
-
-    // Change ourHero's name
     ourHero.changeName(input);
-    
-    // describe the starting situation. Feel free to change this
     System.out.println("You wake up to find yourself in the middle of a forest. \nYou see what looks like a city in the distance. \nWhat would you like to do? \ncity: Go towards the city\nnap: Go back to sleep\n" + ourHero.getName() + ": ");
-
-    // get user input and go to the appropriate zone based on their input
     input = inScanner.nextLine();
     if (input.equals("city")) {
       animals();
@@ -47,17 +37,13 @@ public class TextAdventure
 
   private void animals()
   {
-    // change image
     console.setImage("animals.jpg");
-
-    // describe the area/situation to the user. 
-    // Give them options for choices.
     System.out.println("You walk towards the city and notice some animals along the way.");
     System.out.println("Something isn't right though.");
     System.out.println("They notice you. \nDo you fight them? \nfight: You attempt to fight the animals off\nrun: Run away\n" + ourHero.getName() + ": ");
-    // Take action or go to another zone based on their choice
     String input = inScanner.nextLine();
     double chance = Math.random();
+
     if (input.equals("fight")) {
       if(chance>0.2){
         ourHero.defeatMonster();
@@ -93,16 +79,13 @@ public class TextAdventure
 
   private void forest()
   {
-    // change image
     console.setImage("forest.jpg");
-
-    // describe the area/situation to the user. 
-    // Give them options for choices.
     System.out.println("You returned to the forest.");
     System.out.println("You encounter a magical wolf.");
     System.out.println("They speak to you.");
     System.out.println("Do you respond to them? \nyes: You talk to the wolf about your adventure so far\nno: You walk away, ignoring the wolf\n" + ourHero.getName() + ": ");
     String input = inScanner.nextLine();
+
     if(ourHero.getMonstersDefeated() > 0 && input.compareTo("no") == 0)
     {
       System.out.println("The wolf acknowledges your victory against the animals.");
@@ -120,23 +103,17 @@ public class TextAdventure
       System.out.println("The wolf devours you for your disrespect.");
       gameEnd();
     }
-    // Take action or go to another zone based on their choice
-    // ADD CODE HERE
-    
   }
 
   private void city()
   {
-    // change image
     console.setImage("pumpkintrio.png");
-
-    // describe the area/situation to the user. 
-    // Give them options for choices.
     System.out.println("You enter the city.");
     System.out.println("You encounter dangerous pumpkins.");
     System.out.println("They try to attack you.");
     System.out.println("Do you fight back? \nyes: You fight back\nno: You run away\n" + ourHero.getName() + ": ");
     String input = inScanner.nextLine();
+
     if(ourHero.getMonstersDefeated() > 0 && input.compareTo("no") == 0)
     {
       System.out.println("You escape running with the newfound strength from your previous battles.");
@@ -155,22 +132,16 @@ public class TextAdventure
       System.out.println("The pumpkins easily overpower you due to your inexperience.");
       gameEnd();
     }
-    // Take action or go to another zone based on their choice
-    // ADD CODE HERE
-    
   }
 
   private void nap()
   {
-    // change image
     console.setImage("graveyard.jpg");
-
-    // describe the area/situation to the user. 
-    // Give them options for choices.
     System.out.println("You awake in a graveyard in your dreams.");
     System.out.println("You notice mountains and an ocean in the distance.");
     System.out.println("Where do you explore? \nmountains: You journey to the mountains\nocean: You go towards the ocean\n" + ourHero.getName() + ": ");
     String input = inScanner.nextLine();
+
     if(input.compareTo("mountains") == 0)
     {
       mountains();
@@ -178,20 +149,16 @@ public class TextAdventure
     {
       ocean();
     }
-    // Take action or go to another zone based on their choice
-    // ADD CODE HERE
-    
   }
 
   private void mountains()
   {
-    // change image
-    // ADD CODE HERE
     console.setImage("mountains.png");
     System.out.println("You encounter another traveler.");
     System.out.println("They tell you to travel with them to be safer");
     System.out.println("Do you follow them? \nyes: You travel together\nno: You continue on your own\n" + ourHero.getName() + ": ");
     String input = inScanner.nextLine();
+
     if(input.compareTo("yes") == 0)
     {
       System.out.println("You encounter a bear.");
@@ -207,9 +174,6 @@ public class TextAdventure
       System.out.println("You regret going alone.");
       gameEnd();
     }
-    // Take action or go to another zone based on their choice
-    // ADD CODE HERE
-    
   }
 
   private void ocean()
@@ -220,8 +184,9 @@ public class TextAdventure
     System.out.println("A sea monster attacks the ship at sea.");
     System.out.println("Do you help fight the monster? \nyes: You get on the cannon and shoot the monster\nno: You cower in fear\n" + ourHero.getName() + ": ");
     String input = inScanner.nextLine();
-    double chance = 0.1;
+    double chance = Math.random();
     boolean unlucky = chance < 0.2;
+    
     if(input.compareTo("yes") == 0)
     {
       if(!unlucky)
@@ -244,9 +209,6 @@ public class TextAdventure
       System.out.println("You regret not being brave enough to help.");
       gameEnd();
     }
-    // Take action or go to another zone based on their choice
-    // ADD CODE HERE
-    
   }
 
   private void gameEnd()
